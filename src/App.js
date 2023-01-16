a273c302a3ae2ee1b417ca1951b17c263099a3bc
Marcus Bynoe <marcusbynoe57@gmail.com>
2023-01-16 16:22:25 -0700

    started lab 6

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      city: '',
+      cityData: [],
+      error: false,
+      errorMessage: ''
+    }
+  }
+
+
+  handleInput = (e) => {
+    this.setState({
+      city: e.target.value
+    })
+  }
+
+  getCityData = async (e) => {
+    e.preventDefault();
+
+
+
+    
+
+  }
+
+
+
+
+
+
+
+  render() {
+    return (
+      <>
+        <h1>API Location Calls</h1>
+
+        <form>
+          <label htmlFor="">Search for a City!
+            <input type="text" onInput={this.handleInput} />
+            <button type='submit'>Explore</button>
+          </label>
+
+        </form>
+
+
+
+      </>
+
+
+    )
+  }
 }
 
 export default App;
